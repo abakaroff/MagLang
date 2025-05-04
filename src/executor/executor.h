02b5602c84db902f3b91ff29c8f0def57958bebb
Abakaroff <abakaroffm@gmail.com>
2025-05-04 12:46:34 +0300

    Create executor.h

---
+++ src/executor/executor.h
@@ -0,0 +1,10 @@
+#ifndef EXECUTOR_H
+#define EXECUTOR_H
+
+// Выполняет команду
+void execute_command(const char* input);
+
+// Вычисляет значение выражения
+double evaluate_expr(const char* expr);
+
+#endif
