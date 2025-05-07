19e6d7ca11fca94778c95adae255d3dd4364bc20
Abakaroff <abakaroffm@gmail.com>
2025-05-07 14:25:47 +0300

    Update executor.h

--- src/executor/executor.h
+++ src/executor/executor.h
@@ -1,10 +1,16 @@
 #ifndef EXECUTOR_H
 #define EXECUTOR_H
 
-// Выполняет команду
-void execute_command(const char* input);
+#include "variables.h"
 
-// Вычисляет значение выражения
-double evaluate_expr(const char* expr);
+double evaluate_expression(const char* expr);
+
+void handle_while_loop(const char* input);
+void handle_for_loop(const char* input) ;
+void handle_if_statement(const char* input);
+void handle_print(const char* input);
+void handle_assignment(const char* input);
+void handle_expression(const char* input);
+void execute_command(const char* input);
 
 #endif
